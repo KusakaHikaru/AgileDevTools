7ddc0593c647ac2eab190f730a81f3d3d1047bd9
kaneko <kkaneko@tokyo-gas.co.jp>
2019-01-31 08:02:32 +0900

    SP入力フィールド作成

--- src/app/pages/samples/sample-dash/sample-dash.component.ts
+++ src/app/pages/samples/sample-dash/sample-dash.component.ts
@@ -9,28 +9,35 @@ import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 export class SampleDashComponent {
   private pbls = [
     {
+      id: 'PBL-001',
       title: 'PBL 1',
+      sp: 0,
       position: 1,
       sbls: [{ title: 'SBL 1', position: 1, },{ title: 'SBL 2', position: 2, },]
     },
     {
+      id: 'PBL-002',
       title: 'PBL 2',
+      sp: 0,
       position: 2,
       sbls: [{ title: 'SBL 1', position: 1, },{ title: 'SBL 2', position: 2, },]
     },
     {
+      id: 'PBL-003',
       title: 'PBL 3',
+      sp: 0,
       position: 3,
       sbls: [{ title: 'SBL 1', position: 1, },{ title: 'SBL 2', position: 2, },]
     },
     {
+      id: 'PBL-004',
       title: 'PBL 4',
+      sp: 0,
       position: 4,
       sbls: [{ title: 'SBL 1', position: 1, },{ title: 'SBL 2', position: 2, },]
     },
   ];
 
-  items = ['a', 'b', 'c'];
 
   public drop(event: CdkDragDrop<string[]>, backlogs) {
     moveItemInArray(backlogs, event.previousIndex, event.currentIndex);
